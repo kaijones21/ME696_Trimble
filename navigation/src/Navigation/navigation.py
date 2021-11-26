8f4eb6819a0a62a4feecfe972a3e63b69df83a67
kai <kaijones@hawaii.edu>
2021-11-25 22:27:19 -1000

    Updated to resemble more of a ROS workspace.

--- navigation/src/Navigation/navigation.py
+++ navigation/src/Navigation/navigation.py
@@ -12,8 +12,8 @@ class Navigation:
 
         # State variables
         self.state = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-        self.state_hat = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-        self.prev_time = 0.0
+        self.stateHat = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        self.prevTime = 0.0
 
         # System Matrices 
         self.A = np.identity(6)     # state transition matrix  
@@ -22,14 +22,28 @@ class Navigation:
         self.P_hat = np.identity(6) # estimated process noise covariance matrix
         self.H = np.identity(6)     # sensor transition matrix 
         self.Q = np.identity(6)     # sensor measurement noise
-         
+
+        # Other variables
+        self.gpsData = {'lat': 0.0, 'lon': 0.0, 'alt': 0.0}
+        self.imuData = {'linear_x': 0.0, 'linear_y': 0.0, 'angular_z': 0.0}
+        self.yaw = 0.0
+
+    def set_gps_data(self, gpsData):
+        self.gpsData = gpsData
+
+    def set_imu_data(self, imuData):
+        self.imuData = imuData
+
+    def set_yaw(self, yaw):
+        self.yaw = yaw
+
     def R_matrix(self, psi):
         R = np.array([ [cos(psi), -sin(psi), 0],
                        [sin(psi), cos(psi),  0],
                        [0,        0,         1]])
         return R
 
-    def get_initial_state(self, bodyGPS, mapGPS, yaw):
+    def get_initial_state(self):
         '''
         Initalizes initial state for state estimation. 
 
@@ -38,30 +52,33 @@ class Navigation:
                 mapGPS (dict): contains 'lat', 'lon', and 'alt' of map frame
                 imu (dict): contains heading of body frame w.r.t map frame
         '''
-        
+
         # Assume initial velocities is 0
-        self.state(3) = 0.0
-        self.state(4) = 0.0
-        self.state(5) = 0.0
+        self.state[3] = 0.0
+        self.state[4] = 0.0
+        self.state[5] = 0.0
 
         # Convert GPS to ECEF
         mapECEF = {'x': 0.0, 'y': 0.0, 'z': 0.0}
         bodyECEF = {'x': 0.0, 'y': 0.0, 'z': 0.0}
-        mapECEF['x'], mapECEF['y'], mapECEF['z'] = self.gps_to_ecef_custom(mapGPS['lat'], mapGPS['lon'], mapGPS['alt'])
-        bodyECEF['x'], bodyECEF['y'], bodyECEF['z'] = self.gps_to_ecef_custom(bodyGPS['lat'], bodyGPS['lon'], bodyGPS['alt'])
+        mapECEF['x'], mapECEF['y'], mapECEF['z'] = self.gps2ecef(self.gpsData['lat'], self.gpsData['lon'], self.gpsData['alt'])
+        bodyECEF['x'], bodyECEF['y'], bodyECEF['z'] = self.gps2ecef(self.gpsData['lat'], self.gpsData['lon'], self.gpsData['alt'])
         self.state[0] = bodyECEF['x'] - mapECEF['x']
         self.state[1] = bodyECEF['y'] - mapECEF['y']
 
         # Get yaw
-        self.state[3] = yaw
+        self.state[2] = self.yaw
+        print(self.state)
 
-    def predict(A, B, x, u, Q, P):
-        x_hat = np.dot(A, x) + np.dot(B, u)
-        P_hat = np.dot(A, np.dot(P, A.T)) + Q
+    def predict(self):
+        x_hat = np.dot(self.A, self.state) + np.dot(self.B, self.u)
+        P_hat = np.dot(self.A, np.dot(self.P, self.A.T)) + self.Q
         return (x_hat, P_hat)
 
     def calculateKalmanGain(P, H, R):
         K = np.dot(np.dot(P, H.T), np.linalg.inv((np.dot(H, np.dot(P,H.T)) + R)))  
+        print("Kalman Gain: ")
+        print(K)
         return K 
 
     def update(K, H, P_hat, x_hat, Y): 
@@ -69,44 +86,18 @@ class Navigation:
         x = x_hat + np.dot(K, (Y - np.dot(H, x_hat)))
         return (x, P)
 
-coords = [
-  (37.4001100556,  -79.1539111111,  208.38),
-  (37.3996955278,  -79.153841,  208.48),
-  (37.3992233889,  -79.15425175,  208.18),
-  (37.3989114167,  -79.1532775833,  208.48),
-  (37.3993285556,  -79.1533773333,  208.28),
-  (37.3992801667,  -79.1537883611,  208.38),
-  (37.3992441111,  -79.1540981944,  208.48),
-  (37.3992616389,  -79.1539428889,  208.58),
-  (37.3993530278,  -79.1531711944,  208.28),
-  (37.4001223889,  -79.1538085556,  208.38),
-  (37.3992922222,  -79.15368575,  208.28),
-  (37.3998074167,  -79.1529132222,  208.18),
-  (37.400068,  -79.1542711389,  208.48),
-  (37.3997516389,  -79.1533794444,  208.38),
-  (37.3988933333,  -79.1534320556,  208.38),
-  (37.3996279444,  -79.154401,  208.58),
-]
+    def gps2ecef(self, lat, lon, alt):
+        rad_lat = lat * (pi / 180.0)
+        rad_lon = lon * (pi / 180.0)
 
-def gps_to_ecef_custom(lat, lon, alt):
-    rad_lat = lat * (pi / 180.0)
-    rad_lon = lon * (pi / 180.0)
+        a = 6378137.0
+        finv = 298.257223563
+        f = 1 / finv
+        e2 = 1 - (1 - f) * (1 - f)
+        v = a / sqrt(1 - e2 * sin(rad_lat) * sin(rad_lat))
 
-    a = 6378137.0
-    finv = 298.257223563
-    f = 1 / finv
-    e2 = 1 - (1 - f) * (1 - f)
-    v = a / sqrt(1 - e2 * sin(rad_lat) * sin(rad_lat))
+        x = (v + alt) * cos(rad_lat) * cos(rad_lon)
+        y = (v + alt) * cos(rad_lat) * sin(rad_lon)
+        z = (v * (1 - e2) + alt) * sin(rad_lat)
 
-    x = (v + alt) * cos(rad_lat) * cos(rad_lon)
-    y = (v + alt) * cos(rad_lat) * sin(rad_lon)
-    z = (v * (1 - e2) + alt) * sin(rad_lat)
-
-    return x, y, z
-
-def run_test():
-    for pt in coords:
-        print('custom', gps_to_ecef_custom(pt[0], pt[1], pt[2]))
-
-if __name__ == "__main__":
-    pass
+        return x, y, z
